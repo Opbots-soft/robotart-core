f10c1694e176688896cbae3e81e45e73c790ee0f
mahsheikhdir <mahsheikhdir@gmail.com>
2017-11-04 10:27:03 -0700

    implemented the color version of control with an example test

--- control.py
+++ control.py
@@ -1,29 +1,50 @@
 class Control(object):
-    def __init__(self):
-        pass
 
-    def plan_moves(self, brush_strokes):
-        # Return a list of moves for mechanical control to execute the provided brush strokes
-        print('Control planning moves')
+	red = (-1,-2)
+	blue = (-2,-3)
+	green = (-4,-5)
 
-        data = []
+	def __init__(self):
+		pass
 
-        brush_strokes.sort()
-        for stroke in brush_strokes:
-        	above = (stroke[0], stroke[1], 10)
-        	canvas = (stroke[0], stroke[1], 0)
+	def plan_moves(self, brush_strokes):
+		# Return a list of moves for mechanical control to execute the provided brush strokes
+		print('Control planning moves')
 
-        	print(above)
-        	print(canvas)
-        	data.append(above)
-        	data.append(canvas)
+		data = []
 
-        return data
+		brush_strokes.sort(key=lambda x: x[2][0]) # Sort by color
+		print(brush_strokes)
 
-'''
-# Test
+		color = None
+		for stroke in brush_strokes:
+			
+			if(color != stroke[2]):
+				color = (stroke[2][0], stroke[2][1], 10)
+				inColor = (stroke[2][0], stroke[2][1], 0)
+				data.append(color)
+				data.append(inColor)
+				data.append(color)
+				# print(color)
+				# print(inColor)
+				# print(color)
+
+			above = (stroke[0], stroke[1], 10)
+			canvas = (stroke[0], stroke[1], 0)
+
+			data.append(above)
+			data.append(canvas)
+			data.append(above)
+
+			# print(above)
+			# print(canvas)
+			# print(above)
+
+		return data
+
+
+	# Test
 test = Control()
-color = (1,2)
-strokes = [(4,2,color), (9,3,color), (1,5,color)]
+strokes = [(4,2,test.green),(3,2, test.green), (9,3,test.red), (1,5,test.blue)]
 test.plan_moves(strokes)
-'''
+
