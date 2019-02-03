85177ca59246a554ae937b256c0baa878e3a400a
kaliesophic <aiden.smith10@gmail.com>
2019-02-02 16:05:53 -0800

    added framework for imgproc.py

--- imgproc.py
+++ imgproc.py
@@ -1,8 +1,37 @@
-class ImageProcessing(object):
+class ImageProcessing:
     def __init__(self):
         pass
-
+    
+    
+    # take the provided image (Either an OpenCV mat or Numpy array) and output some kind of abstraction
     def process_image(self, image):
-        # take the provided image (Either an OpenCV mat or Numpy array) and output a list of brush strokes
         print('Image processing running')
-        return []
+        # perform some image processing to 'image'
+        
+        
+        return ImageAbstraction(image)
+    
+    
+    
+    
+class ImageAbstraction:
+    
+    class pointStruct:
+        position = (0.0, 0.0)
+        color = 0
+    
+    def __init__(self, image):
+        
+        self.pointList = []
+        
+        self.generateAbstraction(image)
+    
+    # fill 'pointList' with pointStructs corresponding to the image given
+    def generateAbstraction(self, image):
+        return
+       
+    # return an iterator on the list, so that 'for p in ImageAbstraction(img):' works
+    def __iter__(self):
+        return iter(self.pointList)
+    
+        
