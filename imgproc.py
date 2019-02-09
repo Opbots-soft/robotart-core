72f6083944622e52c2baef344d29747e9cae87a4
kaliesophic <aiden.smith10@gmail.com>
2019-02-08 22:35:32 -0800

    Added generation of pointillism test pattern (grid)

--- imgproc.py
+++ imgproc.py
@@ -1,3 +1,7 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
 class ImageProcessing:
     def __init__(self):
         pass
@@ -8,7 +12,7 @@ class ImageProcessing:
         print('Image processing running')
         # perform some image processing to 'image'
         
-        
+        # Pass the new image to ImageAbstraction class, return the instance
         return ImageAbstraction(image)
     
     
@@ -16,9 +20,11 @@ class ImageProcessing:
     
 class ImageAbstraction:
     
-    class pointStruct:
-        position = (0.0, 0.0)
-        color = 0
+    STEPSIZE = 50
+    
+    class PointStruct:
+        position = (0, 0)
+        color = (0, 0, 0)
     
     def __init__(self, image):
         
@@ -28,10 +34,52 @@ class ImageAbstraction:
     
     # fill 'pointList' with pointStructs corresponding to the image given
     def generateAbstraction(self, image):
-        return
+        
+        dims = image.shape
+        for x in range(0, dims[0], self.STEPSIZE):
+            for y in range(0, dims[1], self.STEPSIZE):
+                newPoint = self.PointStruct()
+                newPoint.position = (x,y)
+                newPoint.color = image[x,y] # Naive approach to pointilism- just get the pixel colors at the sample points.
+                self.pointList.append(newPoint)
        
     # return an iterator on the list, so that 'for p in ImageAbstraction(img):' works
     def __iter__(self):
         return iter(self.pointList)
     
-        
+
+
+
+# Testing code; remove later
+
+img = cv2.imread('test.png', cv2.IMREAD_COLOR)
+
+result = ImageAbstraction(img)
+
+x = []
+y=[]
+color=[]
+
+for ps in result :
+    x.append(ps.position[0])
+    y.append(ps.position[1])
+    color.append(ps.color)
+
+color = list(map((lambda c : [c[2]/256, c[1]/256, c[0]/256]), color))
+# Map from colors that are 0-255 to 0.0-1.0
+# Also go from BGR to RGB
+
+
+print(color)
+
+plt.scatter(y,x, c=color)
+
+plt.title("Pointillism Test", fontsize=19)
+
+plt.xlabel("X value (px)", fontsize=10)
+
+plt.ylabel("Y value (px)", fontsize=10)
+
+plt.tick_params(axis='both', which='major', labelsize=9)
+
+plt.show()
