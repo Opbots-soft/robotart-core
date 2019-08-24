cf09ce4e28699a105921dfbeca5905685755ad59
unknown <kausikkris10@gmail.com>
2019-08-24 07:18:04 -0700

    Changed camera start position

--- delta-sim/sim.js
+++ delta-sim/sim.js
@@ -35,7 +35,7 @@ var instructions = [];
 function setupScene() {
     scene = new THREE.Scene();
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.z = 3;
+    camera.position.z = 7;
 
     renderer = new THREE.WebGLRenderer();
     renderer.shadowMap.enabled = true;
